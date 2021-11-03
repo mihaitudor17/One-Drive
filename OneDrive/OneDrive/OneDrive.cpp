00372f2dc1b5fb707f5ab746019285034092e349
Nutzu000 <glizard.glizard@yahoo.com>
2021-11-03 11:40:36 +0200

    Another commit test for commit counter script

--- OneDrive/OneDrive/OneDrive.cpp
+++ OneDrive/OneDrive/OneDrive.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
     for (int i = 0; i < 10; i++) {
         std::cout<<i << ". Simple commit test for commit counter script\n";
     }
+    std::string input;
+    do {
+        std::getline(std::cin, input);
+        std::cout << input;
+    } while (input!="Close");
 }
