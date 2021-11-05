d657071e63f7e074e7ffb6d6a2770d126361b1d1
Nutzu000 <glizard.glizard@yahoo.com>
2021-11-05 14:44:28 +0200

    Showing all the last write time of the files from a folder

--- OneDrive/OneDrive/OneDrive.cpp
+++ OneDrive/OneDrive/OneDrive.cpp
@@ -8,10 +8,18 @@
 int main()
 {
 	//initialise folder path
-	auto path = std::filesystem::temp_directory_path() / "C:/Users/gliza/Desktop/Synchronized Folder";
+	auto path = "C:/Users/gliza/Desktop/Synchronized Folder 1";
+	auto path2 = "C:/Users/gliza/Desktop/Synchronized Folder 2";
 
+	std::cout << "Folder 1:\n";
 	for (auto& p : std::filesystem::directory_iterator(path)) {
 		std::string fileName = p.path().string();
-		std::cout << fileName << std::endl;
+		std::cout << fileName << " " << std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count() << std::endl;
 	}
+	std::cout << "Folder 2:\n";
+	for (auto& p : std::filesystem::directory_iterator(path2)) {
+		std::string fileName = p.path().string();
+		std::cout << fileName << " " << std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count() << std::endl;
+	}
+
 }
