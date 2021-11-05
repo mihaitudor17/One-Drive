b7cec24279bf4cac080ae9a1353fabaa62ccaa7b
Tinel <tinel_valentin@yahoo.com>
2021-11-05 21:43:35 +0200

    I have added a button and an line edit box.

--- OneDrive/OneDrive/main.cpp
+++ OneDrive/OneDrive/main.cpp
@@ -1,10 +1,15 @@
-#include "OneDrive.h"
+#include "Window.h"
 #include <QtWidgets/QApplication>
-
+#include <iostream>
+#include <QWidget>
+#include <QObject>
+#include <QString>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    OneDrive w;
+    Window w;
+    w.setFixedSize(700, 700);
     w.show();
+
     return a.exec();
 }
