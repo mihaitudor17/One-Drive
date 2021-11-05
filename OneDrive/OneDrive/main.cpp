c42dfdc35f6a0df80ddb272f2a8a1b90a143abc7
Nutzu000 <glizard.glizard@yahoo.com>
2021-11-05 21:56:15 +0200

    Merged GUI with FolderSynchronization

--- OneDrive/OneDrive/main.cpp
+++ OneDrive/OneDrive/main.cpp
@@ -4,12 +4,30 @@
 #include <QWidget>
 #include <QObject>
 #include <QString>
+#include <chrono>
+#include <iomanip>
+#include <fstream>
+#include <filesystem>
+#include <string>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Window w;
     w.setFixedSize(700, 700);
     w.show();
+	//initialise folder path
+	auto path = "C:/Users/gliza/Desktop/Synchronized Folder 1";
+	auto path2 = "C:/Users/gliza/Desktop/Synchronized Folder 2";
 
+	std::cout << "Folder 1:\n";
+	for (auto& p : std::filesystem::directory_iterator(path)) {
+		std::string fileName = p.path().string();
+		std::cout << fileName << " " << std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count() << std::endl;
+	}
+	std::cout << "Folder 2:\n";
+	for (auto& p : std::filesystem::directory_iterator(path2)) {
+		std::string fileName = p.path().string();
+		std::cout << fileName << " " << std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count() << std::endl;
+	}
     return a.exec();
 }
