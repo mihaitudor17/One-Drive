a17ba08e6f11fc67ecc3225083ca72f216e3bc01
Nutzu000 <glizard.glizard@yahoo.com>
2021-11-07 13:47:11 +0200

    Getting the last modified folder

--- OneDrive/OneDrive/main.cpp
+++ OneDrive/OneDrive/main.cpp
@@ -16,18 +16,27 @@ int main(int argc, char *argv[])
     w.setFixedSize(700, 700);
     w.show();
 	//initialise folder path
-	auto path = "./Synchronized Folder 1";
-	auto path2 = "./Synchronized Folder 2";
+	std::string folder1 = "Synchronized Folder 1";
+	std::string folder2 = "Synchronized Folder 2";
+	std::string path1 = "./"+ folder1;
+	std::string path2 = "./"+ folder2;
 	std::ofstream f("Output.txt");
-	f << "Folder 1:\n";
-	for (auto& p : std::filesystem::directory_iterator(path)) {
+	long long lastWrittenTime=0;
+	std::string lastWrittenFolder;
+	for (auto& p : std::filesystem::directory_iterator(path1)) {
 		std::string fileName = p.path().string();
-		f<<fileName << " " << std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count() << std::endl;
+		if (lastWrittenTime < std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count()) {
+			lastWrittenTime = std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count();
+			lastWrittenFolder = folder1;
+		}
 	}
-	f << "Folder 2:\n";
 	for (auto& p : std::filesystem::directory_iterator(path2)) {
 		std::string fileName = p.path().string();
-		f << fileName << " " << std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count() << std::endl;
+		if (lastWrittenTime < std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count()) {
+			lastWrittenTime = std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count();
+			lastWrittenFolder = folder2;
+		}
 	}
+	f << "The last modified folder is: " << lastWrittenFolder;
     return a.exec();
 }
