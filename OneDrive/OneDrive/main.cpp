7955f95be8d3faabe4d5e5f9e19ab492f1f1875c
Nutzu000 <glizard.glizard@yahoo.com>
2021-11-07 13:15:15 +0200

    Added relative path of the folders

--- OneDrive/OneDrive/main.cpp
+++ OneDrive/OneDrive/main.cpp
@@ -16,18 +16,18 @@ int main(int argc, char *argv[])
     w.setFixedSize(700, 700);
     w.show();
 	//initialise folder path
-	auto path = "C:/Users/gliza/Desktop/Synchronized Folder 1";
-	auto path2 = "C:/Users/gliza/Desktop/Synchronized Folder 2";
-
-	std::cout << "Folder 1:\n";
+	auto path = "./Synchronized Folder 1";
+	auto path2 = "./Synchronized Folder 2";
+	std::ofstream f("Output.txt");
+	f << "Folder 1:\n";
 	for (auto& p : std::filesystem::directory_iterator(path)) {
 		std::string fileName = p.path().string();
-		std::cout << fileName << " " << std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count() << std::endl;
+		f<<fileName << " " << std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count() << std::endl;
 	}
-	std::cout << "Folder 2:\n";
+	f << "Folder 2:\n";
 	for (auto& p : std::filesystem::directory_iterator(path2)) {
 		std::string fileName = p.path().string();
-		std::cout << fileName << " " << std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count() << std::endl;
+		f << fileName << " " << std::chrono::duration_cast<std::chrono::milliseconds>(p.last_write_time().time_since_epoch()).count() << std::endl;
 	}
     return a.exec();
 }
