bc807799d270dbe9736097b5222289f318027208
Tinel <tinel_valentin@yahoo.com>
2021-11-13 13:40:34 +0200

    Added a functionality to the login button that opens a new frame on click.

--- OneDrive/OneDrive/Window.cpp
+++ OneDrive/OneDrive/Window.cpp
@@ -1,6 +1,7 @@
 #include "Window.h"
 #include <QMessageBox>
 #include <QLineEdit>
+#include "WindowAccount.h"
 
 void Window::on_Button_clicked() // on click i take the username and store it
 {
@@ -9,6 +10,11 @@ void Window::on_Button_clicked() // on click i take the username and store it
         std::string username = editUsername->text().toStdString();
 
         label->setText("Name saved");
+        WindowAccount* back = new WindowAccount(this);
+        back->setFixedSize(700, 700);
+        back->show();
+        this->close();
+
     }
     else
         label->setText("Introduceti scris");
@@ -35,7 +41,7 @@ Window::Window(QWidget* parent)
     setCentralWidget(ui_area);
     QPushButton* login, * signUp;
 
-    QHBoxLayout* mainLayout;
+    QHBoxLayout* mainLayout,* buttonLayout;
     QVBoxLayout* secondLayout;
 
     setWindowTitle("Window test");
@@ -46,6 +52,10 @@ Window::Window(QWidget* parent)
     login = new QPushButton("Login");
     editUsername = new QLineEdit();
     label = new QLabel();
+    QLabel* welcome = new QLabel("Welcome to OneDrive!");
+    welcome->setStyleSheet("font-size:30px;font-weight:bold;");
+    
+    setWindowTitle("Account Window");
 
     login->setFixedSize(100, 20);
     login->setStyleSheet("border-radius:10px;background-color:grey;border:3 px solid black;");
@@ -56,13 +66,16 @@ Window::Window(QWidget* parent)
     editUsername->setMaximumSize(300, 20);
     mainLayout = new QHBoxLayout(this);
     secondLayout = new QVBoxLayout(this);
+    buttonLayout = new QHBoxLayout(this);
 
-
+    secondLayout->addWidget(welcome);
     secondLayout->addWidget(labelUsername);
     secondLayout->addWidget(editUsername);
 
-    secondLayout->addWidget(login);
-    secondLayout->addWidget(signUp);
+    
+    buttonLayout->addWidget(login);
+    buttonLayout->addWidget(signUp);
+    secondLayout->addLayout(buttonLayout);
     secondLayout->addWidget(label);
     mainLayout->addLayout(secondLayout, Qt::AlignCenter);
 
---
+++ OneDrive/OneDrive/WindowAccount.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <QtWidgets/QMainWindow>
+#include "ui_OneDrive.h"
+#include <QVBoxLayout>
+#include <QLabel>
+
+class WindowAccount : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    WindowAccount(QWidget* parent = 0);
+
+private:
+
+
+};
