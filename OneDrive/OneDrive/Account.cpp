17e2d1868ee5c9e656d8f114fc0d8e09268513f9
Nutzu000 <glizard.glizard@yahoo.com>
2021-11-27 19:19:53 +0200

    Checking the type of file from the folder.

--- OneDrive/OneDrive/Account.cpp
+++ OneDrive/OneDrive/Account.cpp
@@ -3,29 +3,53 @@
 
 void Account::showContentsOfFile()
 {
-	QString filename = "./folderIcon.png";
-	int widthAdjust = -30;
-	std::filesystem::path path= "./StoredFiles/"+userName;
-	//std::filesystem::path path = "C:/Users/40763/Desktop/OneDrive/OneDrive/OneDrive/StoredFiles/tinel";
-	for (auto& file:std::filesystem::directory_iterator(path)) 
-	{
-		widthAdjust += 30;
-		QLabel* image = new QLabel(ui.frame);
-		QPixmap pix;
-		int coordinatesX = image->x();
-		int coordinatesY = image->y();
-		int coordinatesWidth = image->width();
-		int coordinatesHeight = image->height();
-		/** to check wether load ok */
-		if (pix.load(filename)) {
-			/** scale pixmap to fit in label'size and keep ratio of pixmap */
-			pix = pix.scaled(image->size(), Qt::KeepAspectRatio);
-			image->setPixmap(pix);
-			image->setGeometry(coordinatesX , coordinatesY + widthAdjust, coordinatesWidth, coordinatesHeight);
-			
-		}
 
-	}
+    int widthAdjust = -30;
+    QPixmap pix;
+    std::filesystem::path path = "./StoredFiles/" + userName;
+
+    for (auto& file : std::filesystem::directory_iterator(path))
+    {
+        widthAdjust += 30;
+        QLabel* image = new QLabel(ui.frame);
+        int coordinatesX = image->x();
+        int coordinatesY = image->y();
+        int coordinatesWidth = image->width();
+        int coordinatesHeight = image->height();
+        if (std::filesystem::is_directory(file)) {
+            QString filename = "./folderIcon.png";
+            if (pix.load(filename)) {
+
+                pix = pix.scaled(image->size(), Qt::KeepAspectRatio);
+                image->setPixmap(pix);
+                image->setGeometry(coordinatesX, coordinatesY + widthAdjust, coordinatesWidth, coordinatesHeight);
+
+            }
+        }
+        else {
+            if (file.path().filename().string().find(".txt") != std::string::npos) {
+                QString filename = "./fileIcon.jpg";
+                if (pix.load(filename)) {
+
+                    pix = pix.scaled(image->size(), Qt::KeepAspectRatio);
+                    image->setPixmap(pix);
+                    image->setGeometry(coordinatesX, coordinatesY + widthAdjust, coordinatesWidth, coordinatesHeight);
+
+                }
+
+            }
+            else if (file.path().filename().string().find(".jpg") != std::string::npos || file.path().filename().string().find(".png") != std::string::npos) {
+                std::cout << "Este o poza!" << std::endl;
+            }
+            else if (file.path().filename().string().find(".mp4") != std::string::npos) {
+                std::cout << "Este un film!" << std::endl;
+            }
+            else {
+                std::cout << "Este un alt tip de fisier!" << std::endl;
+            }
+        }
+
+    }
 }
 
 Account::Account(const std:: string &userName ,QWidget *parent)
