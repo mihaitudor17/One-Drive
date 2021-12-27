b2714acb2124149fbc85c8f4bfc8e34cd13833d5
Mihai_Tudor17 <mihai.blaga@student.unitbv.ro>
2021-12-27 13:07:17 +0200

    Added the Server class

---
+++ OneDrive/TCP_Server/Server.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <ws2tcpip.h>
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#define BUFFER_SIZE 1024
+class Server
+{
+private:
+	SOCKET clientSock;
+public:
+	bool connectClient();
+	char* recvFileName(SOCKET clientSock);
+	bool sendFileName(SOCKET clientSock, char fileRequested[FILENAME_MAX]);
+	long recvFileSize(SOCKET clientSock);
+	bool sendFileSize(SOCKET clientSock, long fileSize);
+	bool sendFile(SOCKET clientSock, std::string path);
+	bool writeToFile(SOCKET clientSock, std::ofstream file, std::string fullPath, int fileRequestedsize);
+	char* recvUser(SOCKET clientSock);
+	Server() {}
+	SOCKET getSock()
+	{
+		return clientSock;
+	}
+	void setSock(SOCKET clientSock)
+	{
+		this->clientSock = clientSock;
+	}
+};
