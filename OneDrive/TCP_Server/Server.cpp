d0587b1a62b1c459ada66b796f517362f3344812
Mihai_Tudor17 <mihai.blaga@student.unitbv.ro>
2021-12-27 13:09:40 +0200

    Remade the function to connect to the client

--- OneDrive/TCP_Server/Server.cpp
+++ OneDrive/TCP_Server/Server.cpp
@@ -7,3 +7,36 @@ bool Server::connectClient()
 		std::cerr << "Error starting winsock!" << std::endl;
 		return -1;
 	}
+	sockaddr_in listenerHint;
+	listenerHint.sin_family = AF_INET;
+	listenerHint.sin_port = htons(55000);
+	listenerHint.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	bind(listenerSock, (sockaddr*)&listenerHint, sizeof(listenerHint));
+	listen(listenerSock, SOMAXCONN);
+
+	sockaddr_in clientHint;
+	int clientSize = sizeof(clientHint);
+
+	SOCKET clientSock = accept(listenerSock, (sockaddr*)&clientHint, &clientSize);
+
+	if (clientSock == SOCKET_ERROR) {
+		std::cerr << "Error accept socket! " << WSAGetLastError() << std::endl;
+		closesocket(listenerSock);
+		WSACleanup();
+		return -1;
+	}
+
+	char host[NI_MAXHOST];
+	char serv[NI_MAXSERV];
+
+	if (getnameinfo((sockaddr*)&clientHint, sizeof(clientHint), host, NI_MAXHOST, serv, NI_MAXSERV, 0) == 0) {
+		std::cout << "Host: " << host << " connected on port: " << serv << std::endl;
+	}
+	else {
+		inet_ntop(AF_INET, &clientHint.sin_addr, host, NI_MAXHOST);
+		std::cout << "Host: " << host << " connected on port: " << ntohs(clientHint.sin_port) << std::endl;
+	}
+	closesocket(listenerSock);
+	this->clientSock = clientSock;
+}
