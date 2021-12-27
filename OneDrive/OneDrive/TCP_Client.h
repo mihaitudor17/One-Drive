89abb6f09d14219f5161950c33f498e92d32e537
Mihai_Tudor17 <mihai.blaga@student.unitbv.ro>
2021-12-27 12:56:00 +0200

    Created class Client

--- OneDrive/OneDrive/TCP_Client.h
+++ OneDrive/OneDrive/TCP_Client.h
@@ -8,14 +8,30 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <filesystem>
-
-
 #pragma comment (lib, "Ws2_32.lib")
 #pragma comment (lib, "Mswsock.lib")
 #pragma comment (lib, "AdvApi32.lib")
-
-
-#define DEFAULT_BUFLEN 64000
-#define DEFAULT_PORT "27015"
-int client();
+#define BUFFER_SIZE 1024
+class Client {
+public:
+	Client();
+	SOCKET getSock()
+	{
+		return clientSock;
+	}
+	void setSock(SOCKET tempSock)
+	{
+		clientSock = tempSock;
+	}
+	bool sendFileName(SOCKET clientSock, char fileRequested[FILENAME_MAX]);
+	char* recvFileName(SOCKET clientSock);
+	long recvFileSize(SOCKET clientSock);
+	bool sendFileSize(SOCKET clientSock, long fileRequestedsize);
+	bool writeToFile(SOCKET clientSock, std::string fullPath, int fileRequestedsize);
+	bool sendFile(SOCKET clientSock, std::ifstream file);
+	bool sendUser(SOCKET clientSock, char username[FILENAME_MAX]);
+	bool connectServer();
+private:
+	SOCKET clientSock;
+};
 
