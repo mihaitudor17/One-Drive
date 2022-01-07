aea975ceb0f839b836e3076be4f5dd4ca5e04946
Nutzu000 <glizard.glizard@yahoo.com>
2022-01-07 12:27:44 +0200

    Added getters for metadata

--- OneDrive/OneDrive/Metadata.cpp
+++ OneDrive/OneDrive/Metadata.cpp
@@ -4,6 +4,27 @@ Metadata::Metadata()
 {
 }
 
+void Metadata::setPath(const std::filesystem::path& path)
+{
+	m_path = path;
+}
+
 Metadata::Metadata(const std::filesystem::path& path):m_path{path}
 {
+
+}
+
+void Metadata::setBody(const nlohmann::json& body)
+{
+	m_body = body;
+}
+
+std::filesystem::path Metadata::getPath() const
+{
+	return m_path;
+}
+
+nlohmann::json Metadata::getBody() const
+{
+	return m_body;
 }
